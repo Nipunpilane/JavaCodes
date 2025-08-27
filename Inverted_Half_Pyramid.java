dd2039b810fa0d068791653172c18ddce0508a09
Nipun <nipunpilane@gmail.com>
2025-08-27 23:14:13 +0530

    Inital commit

---
+++ Inverted_Half_Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Inverted_Half_Pyramid {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Number To form Inverted Half Triangle");
+        int inver =sc.nextInt();
+        for(int i=1;i<=inver;i++){
+
+            for(int j=1;j<=inver;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");    
+            }
+            System.out.println();  
+        }  
+    }
+}
+
--- Patten_Line.java
+++ Patten_Line.java
@@ -1,10 +1,23 @@
 import java.util.*;
 public class Patten_Line {
-    public static void main(String args[])[]{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Number To Print");
-        
         int patten= sc.nextInt();
+        
+        for(int i=1;i<=patten;i++){
+         for(int j=1;j<=patten;j++){
+               System.out.print("*");
+         }
+         System.out.println();
+         
+        }
 
     }
 }
+
+//*****
+// *****
+// *****
+// *****
+// *****
